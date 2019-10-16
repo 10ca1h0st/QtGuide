7bd3725c70f1039b2d829b390386962666743d11
10ca1h0st <1114802868@qq.com>
2019-10-16 16:15:19 +0800

    change result.ui

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
-import sys
+from PyQt5.QtCore import pyqtSlot
+import sys,json
 
 from clickableWidget import ClickableQLabel
 
 
-class Ui(QtWidgets.QMainWindow):
+class Ui(QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('search.ui', self)
-
-        self.button = self.findChild(QPushButton, 'searchButton')
-        self.button.clicked.connect(self.search)
 
         self.lineedit = self.findChild(QLineEdit,'searchLineEdit')
 
@@ -22,26 +20,46 @@ class Ui(QtWidgets.QMainWindow):
 
         self.show()
     
+    @pyqtSlot
     def search(self):
         self.hide()
         self.r = Result()
-        self.r.showContent(self)
+        self.r.showContent(self,self.lineedit.text())
 
 
-class Result(QtWidgets.QDialog):
+class Result(QWidget):
     def __init__(self):
         super(Result,self).__init__()
         uic.loadUi('result.ui',self)
         self.setWindowTitle('RESULT')
     
-    def showContent(self,parent):
+    def showContent(self,parent,keyWord):
         self.parent = parent
         self.show()
     
+    def getContent(self,keyWord):
+        keyWord = keyWord+'.json'
+        f = open(keyWord,encoding='utf-8')
+        content = f.read()
+        content = json.loads(content)
+        content = content['data']
+
+    def constructTable(content):
+        pass
+
     def closeEvent(self,event):
         if self.parent.isHidden():
             self.parent.setHidden(False)
 
+
+'''
+def handleJson(name):
+    name = name+'.json'
+    f = open(name,encoding='utf-8')
+    content = f.read()
+    content = json.loads(content)
+    #print(content)
+'''
 
 
 if __name__ == '__main__':
