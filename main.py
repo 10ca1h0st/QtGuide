195ed69fc7c83d3593bf972d6e874406492cd1e3
wujiaming <1114802868@qq.com>
2019-10-16 18:47:51 +0800

    create result.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot,pyqtSignal
 import sys,json
 
+from result import Result
 from clickableWidget import ClickableQLabel
 
 
 class Ui(QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi('search.ui', self)
+        uic.loadUi('./UI/search.ui', self)
 
         self.lineedit = self.findChild(QLineEdit,'searchLineEdit')
 
@@ -25,83 +26,6 @@ class Ui(QMainWindow):
         self.hide()
         self.r = Result()
         self.r.showContent(self,self.lineedit.text())
-
-
-class Result(QWidget):
-    def __init__(self):
-        super(Result,self).__init__()
-        uic.loadUi('result.ui',self)
-        self.setWindowTitle('RESULT')
-
-        self.table = self.findChild(QTableWidget,'content')
-
-        self.pre = self.findChild(QPushButton,'pre')
-        self.next = self.findChild(QPushButton,'next')
-
-        self.pre.clicked.connect(self.prePage)
-        self.next.clicked.connect(self.nextPage)
-    
-    @pyqtSlot()
-    def prePage(self):
-        pass
-
-    @pyqtSlot()
-    def nextPage(self):
-        pass
-    
-    def showContent(self,parent,keyWord):
-        self.parent = parent
-        if self.getContent(keyWord):
-            self.show()
-        else:
-            self.parent.setHidden(False)
-    
-
-    #return None means error
-    def getContent(self,keyWord):
-        keyWord = keyWord+'.json'
-        try:
-            f = open(keyWord,encoding='utf-8')
-        except FileNotFoundError:
-            QMessageBox.critical(self,"错误","没有发现有效信息")
-            return
-        content = f.read()
-        content = json.loads(content)
-        self.content = content['data']
-        self.constructTable()
-        return 1
-
-    def constructTable(self):
-        self.item_num = len(self.content)
-        column_num = len(self.content[0].keys())
-        row_num = 10
-
-        self.table.setRowCount(row_num)
-        self.table.setColumnCount(column_num)
-
-        #set the name of each column
-        self.table.setHorizontalHeaderLabels(self.content[0].keys())
-        #set table not editable
-        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        #set vertical header invisible
-        #self.table.verticalHeader().setVisible(False)
-
-        self.table.resizeColumnsToContents()
-        self.table.resizeRowsToContents() 
-
-    def closeEvent(self,event):
-        if self.parent.isHidden():
-            self.parent.setHidden(False)
-
-
-'''
-def handleJson(name):
-    name = name+'.json'
-    f = open(name,encoding='utf-8')
-    content = f.read()
-    content = json.loads(content)
-    #print(content)
-'''
 
 
 if __name__ == '__main__':
